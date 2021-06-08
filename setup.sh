8f110b4be137fe0714b97847edebf24a08689400
Nur Hikari <nrudesu@gmail.com>
2021-06-08 11:06:29 +0700

    Update folder location in script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt install openbox firefox tightvncserver
-mkdir .vnc
-cat << EOF > .vnc/xstartup
+mkdir ~/.vnc
+cat << EOF > ~/.vnc/xstartup
 #!/bin/bash
 exec openbox
 EOF
-chmod +x .vnc/xstartup
+chmod +x ~/.vnc/xstartup
