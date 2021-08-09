a909cc19b55bd0742103cd6b01d1d4471edcdaab
Nur Hikari <nrudesu@gmail.com>
2021-08-09 20:09:30 +0700

    Add gdown script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get -y install openbox firefox tightvncserver
+sudo apt-get -y install openbox firefox tightvncserver python3-pip
+pip install gdown
 mkdir ~/.vnc
 cat << EOF > ~/.vnc/xstartup
 #!/bin/bash
